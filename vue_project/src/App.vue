b03b832631e308ba9a1b3ba7059c1430cb103b14
ars1208 <ara.kaz1208@gmail.com>
2020-10-11 22:28:26 +0900

    Iintial commit

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -2,26 +2,17 @@
   <div>
     <myheader></myheader>
     <toppage></toppage>
-    <aboutme></aboutme>
-    <myskills></myskills>
-    <contactme></contactme>
   </div>
 </template>
 
 <script>
 import myheader from './components/myheader'
 import toppage from './components/topPage'
-import aboutme from './components/aboutMe'
-import myskills from './components/mySkills'
-import contactme from './components/contactMe'
 
 export default {
   components: {
     myheader,
-    toppage,
-    aboutme,
-    myskills,
-    contactme
+    toppage
   }
 }
 </script>
