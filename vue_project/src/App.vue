6af19f01e50a3fbbf1ac91b496b52faaa0a32cac
ars1208 <ara.kaz1208@gmail.com>
2020-10-11 22:44:58 +0900

    Inital commit

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <myheader></myheader>
     <toppage></toppage>
   </div>
 </template>
 
 <script>
-import myheader from './components/myheader'
 import toppage from './components/topPage'
 
 export default {
   components: {
-    myheader,
     toppage
   }
 }
